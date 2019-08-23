e6c513eca363469c8bbdb798bd98edf422a1042c
BluEyesWhiteJason <jason.speet@gmail.com>
2019-08-22 21:39:00 -0400

    initial commit

---
+++ POSTerminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POSTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World heeelo !");
+        }
+    }
+}
