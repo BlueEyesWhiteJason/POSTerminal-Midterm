1d2facdbbe08090da2a2fb33f7170e77de63844e
moise <ingabiremo20@gmail.com>
2019-08-26 21:43:11 -0400

    Add Product class and a welcome message to the user

---
+++ POSTerminal/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POSTerminal
+{
+   public  class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+         List<Product> productList = new List<Product>();
+        public Product(string name, string description, string category, double price)
+        {
+           this.Name = name;
+            this.Description = description;
+            this.Category = category;
+            this.Price = price;
+        }
+   
+        public  List<Product> RemoveShoeFromList(List<Product> myList, int index)
+        {
+            myList.RemoveAt(index);
+            return myList;
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Description} {Category} ${Price}";
+        }
+
+
+    }
+}
--- POSTerminal/Program.cs
+++ POSTerminal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace POSTerminal
 {
@@ -6,7 +7,20 @@ namespace POSTerminal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World heeelo !");
+           
+            Console.WriteLine("Hello! Welcome to KMJ Shoe shop");
+            List<Product> productList = new List<Product>();
+
+            productList.Add(new Product("Sneakers", "Sport use", "Sports", 37.99));
+            productList.Add(new Product("Mens Boot", "Summer use", "Hicking", 60.99));
+            for (int i = 0; i < productList.Count; i++)
+            {
+                Console.WriteLine($"{i}. " + productList[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Here is Our Shoes ");
+          
+
         }
     }
 }
