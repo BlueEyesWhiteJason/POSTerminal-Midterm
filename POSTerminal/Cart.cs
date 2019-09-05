f675f138d411d490b79641fac3dcea5d96284315
BluEyesWhiteJason <jason.speet@gmail.com>
2019-09-04 21:27:13 -0400

    decimal to double

---
+++ POSTerminal/Cart.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POSTerminal
+{
+    public class Cart : Product
+    {
+        public int Quantity { get; set; }
+        public double Total { get; set; } = 0;
+        public Cart(string name, string description, string category, double price, int quantity) : base(name, description, category, price)
+        {
+            this.Quantity = quantity;
+            Total = quantity * price;
+        }
+
+        public Cart()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $" {Quantity} {Name}  ${Price} ${Total} ";
+        }
+
+        public double CulateTotalPrice(List<Cart> myCart)
+        {
+
+            double totalPrice = 0;
+            
+            foreach (var product in myCart)
+            {
+                totalPrice += product.Total;
+            }
+
+            double salesTax = totalPrice * 0.06;
+            double grandTotal = totalPrice - salesTax;
+            Console.WriteLine("Subtotal: {0:c}", totalPrice);
+            Console.WriteLine("Total sales Tax: {0:c}", salesTax);
+            Console.WriteLine("Grand Total {0:c}", grandTotal);
+            return grandTotal;
+        }
+        public void DisplayCart(List<Cart> myCart)
+        {
+
+            for (int i = 0; i < myCart.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}" + myCart[i]);
+
+            }
+        }
+        public List<Cart> RemoveShoeFromList(List<Cart> myCartList, int index)
+        {
+            myCartList.RemoveAt(index - 1);
+            return myCartList;
+        }
+
+    }
+}
--- POSTerminal/Program.cs
+++ POSTerminal/Program.cs
@@ -1,15 +1,120 @@
-﻿using System;
+using System;
+
 using System.Collections.Generic;
 
 namespace POSTerminal
 {
     class Program
     {
+
+        public static bool answer = true;
         static void Main(string[] args)
         {
+            //string answer;
+            int a;
+            Cart c = new Cart();
+            Product p = new Product();
+            Console.WriteLine("Hello! Welcome to KMJ Shoe shop");
+            List<Product> productList = new List<Product>();
+            List<Product> myProductList = p.GetProductsList(productList);
+            for (int i = 0; i < myProductList.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. " + myProductList[i]);
+            }
+            Console.WriteLine();
+
+            while (answer == true)
+            {
+                try
+                {
+
+                    string input;
+                    double price = 0;
+                    int quantity = 0;
+                    double total = 1;
+                    List<Cart> myCart = new List<Cart>();
+                    while (true)
+                    {
+                        Console.WriteLine("Choose shoe number to be Added To Cart");
+                        Console.WriteLine();
+                        input = Console.ReadLine().Trim().ToLower();
+                        Console.WriteLine("Enter quantity: ");
+                        a = Convert.ToInt32(input) - 1;
+                        quantity = Convert.ToInt32(Console.ReadLine());
+
+                        total = quantity * myProductList[a].Price;
+                        price += quantity * (myProductList[a].Price); // accessing price of shoe                        
+                        myCart.Add(new Cart($"{myProductList[a].Name}", $"{myProductList[a].Description}", $"{myProductList[a].Category}", myProductList[a].Price, quantity));
+                        
+                        c.DisplayCart(myCart);
+                        //   c.CulateTotalPrice(myCart);                      
+                        bool goodAnswer = false;
+                        string response;
+                        while (goodAnswer == false)
+                        {
+                            Console.WriteLine("Would you like to continue? (yes/no)");
+                            response = Console.ReadLine().Trim().ToLower();
+                            if (response == "yes")
+                            {
+                                break;
+                            }
+                            else if (response == "no")
+                            {
+                                answer = false;
+                                Console.WriteLine("Cart Details");
+                                c.DisplayCart(myCart);
+                                c.CulateTotalPrice(myCart);
+                                while (true)
+                                {
+                                    Console.WriteLine("do you wish to remove any item?(y/n)");
+                                    string rm = Console.ReadLine().Trim().ToLower();
+                                    if (rm == "y")
+                                    {
+                                        Console.WriteLine("Enter item number");
+                                        int rmv = Convert.ToInt32(Console.ReadLine());
+                                        c.RemoveShoeFromList(myCart, rmv);
+                                        Console.WriteLine("Cart Details");
+                                        c.DisplayCart(myCart);
+                                        c.CulateTotalPrice(myCart);
+                                        break;
+                                    }
+                                    if (rm == "n")
+                                    {
+                                        Console.WriteLine("Cart Details");
+                                        c.DisplayCart(myCart);
+                                        c.CulateTotalPrice(myCart);
+                                        double x = c.CulateTotalPrice(myCart); // total to be passed to payment method                                        
+                                        MakePayment(x);
 
 
-            
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Wrong input");
+                                        break;
+                                    }
+                                    Console.ReadLine();
+                                    Console.WriteLine();
+                                    break;
+                                }
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Unkwown Answer!!!");
+                                continue;
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            }
         }
 
         public static void MakePayment(double total)
@@ -62,6 +167,7 @@ namespace POSTerminal
             Console.Clear();
 
             c.PrintToReceipt(total);
+
         }
     }
 }
